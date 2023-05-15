d6d883c9f611b25fb3014811fb61b09bede84d33
Yan <i@yinyan.fr>
2023-05-15 18:08:22 +0200

    chore: project setup

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # img-down
+
 Yet another CLI tool to batch compress / downscale images.
+
+TODO
---
+++ src/tests/index.test.ts
@@ -0,0 +1,5 @@
+describe('Library', () => {
+  it('should compress images', () => {
+    expect(true).toBeTruthy();
+  });
+});
